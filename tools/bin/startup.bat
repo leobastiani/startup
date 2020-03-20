b50eac316e3cf4d29579de46f9ec70d7d0287f2f
Leobastiani <leogbastiani@gmail.com>
2020-03-19 21:05:05 -0300

    First commit

---
+++ tools/bin/startup.bat
@@ -0,0 +1,5 @@
+explorer %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+regjump "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run"
+pause
+regjump "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
+pause
