439d584d73745677e7fa249b02c7c15b95342570
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-10-05 21:04:57 -0300

    Add files via upload

--- tools/bin/startup.bat
+++ tools/bin/startup.bat
@@ -1,4 +1,5 @@
 explorer %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+pause
 regjump "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run"
 pause
 regjump "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
